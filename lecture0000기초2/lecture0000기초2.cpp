264b0b70740803ffc9ee05c9397e02cd03494a2d
KIMDONGGYUN <nabilera1@naver.com>
2019-09-24 12:28:24 +0900

    09241228

---
+++ lecture0000기초2/lecture0000기초2.cpp
@@ -0,0 +1,12 @@
+﻿//Ctrl + F7,  Ctrl + B, Ctrl + F5
+
+#include<stdio.h>
+
+int main()
+{
+	float a, b;
+	//scanf_s("%f %e", &a, &b, sizeof(a), sizeof(b));
+	scanf_s("%f %e", &a, &b);
+	printf("a=%f, b=%e\n", a, b);
+	return 0;
+}
